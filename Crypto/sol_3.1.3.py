4991431d6150e4cbe9bf17be5dc1021d51d52291
dipayan2 <dipayan2@illinois.edu>
2020-10-24 11:14:05 -0500

    3.1.3 -- done

---
+++ Crypto/sol_3.1.3.py
@@ -0,0 +1,34 @@
+from Crypto.Cipher import AES
+import sys
+
+if len(sys.argv) < 5:
+    print("Insufficient arguements")
+    exit()
+
+cipher_file = str(sys.argv[1])
+key_file = str(sys.argv[2])
+iv_file = str(sys.argv[3])
+output_file = str(sys.argv[4])
+
+key=""
+with open(key_file) as f:
+    file_contents = f.read().strip()
+    key = bytes.fromhex(file_contents)
+
+iv=""
+with open(iv_file) as f:
+    file_contents = f.read().strip()
+    iv = bytes.fromhex(file_contents)
+
+ciphertext=""
+with open(cipher_file) as f:
+    file_contents = f.read().strip()
+    ciphertext = bytes.fromhex(file_contents)
+
+cipher = AES.new(key, AES.MODE_CBC, iv)
+outText = cipher.decrypt(ciphertext)
+outText = outText.decode().strip()
+
+file = open(output_file,'w')
+file.write(outText)
+file.close()
