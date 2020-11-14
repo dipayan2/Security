8d9b8839164429845172b9c84b4b4cda2d99f6b3
Rio Martinez <rioam2@illinois.edu>
2020-11-14 15:43:36 -0600

    checkpoint 1 done

--- NetSec/cp1.3.synscan.py
+++ NetSec/cp1.3.synscan.py
@@ -13,4 +13,14 @@ if __name__ == "__main__":
     my_ip = get_if_addr(sys.argv[1])
     
     # SYN scan
+    for p in range(1, 1025):
+        if (p*8)%1024==0:
+            print(f"# {p*100//1024}% complete with scan")
+        packet = IP(src=my_ip, dst=ip_addr) / TCP(sport=1234, dport=p, flags="S")
+        resp = sr1(packet, verbose=0, timeout=.22)
+        if resp and (TCP in resp) and (resp[TCP].flags.S and resp[TCP].flags.A):
+            rstPacket = IP(src=my_ip, dst=ip_addr) / TCP(sport=1234, dport=p, flags="R")
+            send(rstPacket, verbose=0)
+            print(f"{ip_addr},{p}")
+
 
